92db4263938252e26e5f7bf6e4523933ba911a95
MrFishPL <karpik-junior@o2.pl>
2022-02-15 15:01:55 +0100

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,14 @@ function menu() {
     // Sprawdza czy trochę zescrollowano, a jeżeli tak to nadaje białe tło dla menu
     if ($(window).scrollTop()==0 && $(".menu").hasClass("scrolled") && !$(".responsive-menu").hasClass("made")) $(".menu").removeClass("scrolled");
     else if ($(window).scrollTop()!=0 && !$(".menu").hasClass("scrolled")) $(".menu").addClass("scrolled");
+
+    if ($(window).scrollTop()==0 && $(".scroll-progress").hasClass("progress-scrolled")) $(".scroll-progress").removeClass("progress-scrolled");
+    else if ($(window).scrollTop()!=0 && !$(".scroll-progress").hasClass("progress-scrolled")) $(".scroll-progress").addClass("progress-scrolled");
   });
 
   $('.burger').click(function () {
     // Otwiera responsywne menu
-    
+
     if (!$(".responsive-menu").hasClass("made")) $(".responsive-menu").addClass("made");
     else $(".responsive-menu").removeClass("made");
 
